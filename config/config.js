b9cc731c410bd114f172fd3ca104d787afb76898
monarchxyspider <raffaysweet@gmail.com>
2026-07-26 15:38:51 +0500

    Create config/config.js

---
+++ config/config.js
@@ -0,0 +1,53 @@
+module.exports = {
+
+    prefix: "S!",
+
+    botName: "ShadeMist",
+
+    embedColor: "#E53935",
+
+    supportServer: "https://discord.gg/54vJcse3",
+
+    developers: [
+        "1445544845002276944"
+    ],
+
+    emojis: {
+
+        success: "<:green_tick:1530887008581587095>",
+
+        error: "<:Cancel:1480823821765443737>",
+
+        target: "<:target_mist:1488443659182669824>",
+
+        time: "<:time_mist:1488445805319946350>",
+
+        owner: "<:owner_mist:1488443589468880986>",
+
+        user: "<:infouser_mit:1488443245116653608>",
+
+        message: "<:message_mist:1488443143408980050>",
+
+        list: "<:list:1494365866916053022>",
+
+        fetch: "<:fetch_message_mist:1488442927221837885>",
+
+        mute: "<:mute:1488895692784275630>",
+
+        moderator: "<:Moderator_mist:1488443533013684325>",
+
+        sword: "<:sword:1486605962809704520>",
+
+        gear: "<:Gear:1481128756826472569>",
+
+        staff: "<:red_staff:1484584155273625650>",
+
+        stats: "<:stats:1486020690690703555>",
+
+        member: "<:member:1481129830899519700>",
+
+        timeout: "<:timeout:1481133273949405305>",
+
+        place: "<:wplace:1486604975848161330>"
+    }
+};
