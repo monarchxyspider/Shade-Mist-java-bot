082050b573e4e81cda3429af888f952e2f32dcbb
monarchxyspider <raffaysweet@gmail.com>
2026-07-26 15:53:21 +0500

    Update eventHandler.js

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -1,22 +1,22 @@
 const fs = require("fs");
-const path = require("path");
 
 module.exports = (client) => {
-    const eventsPath = path.join(__dirname, "..", "events");
 
-    const files = fs
-        .readdirSync(eventsPath)
+    const eventFiles = fs
+        .readdirSync("./events")
         .filter(file => file.endsWith(".js"));
 
-    for (const file of files) {
-        const event = require(path.join(eventsPath, file));
+    for (const file of eventFiles) {
+
+        const event = require(`../events/${file}`);
 
         if (event.once) {
-            client.once(event.name, (...args) => event.execute(...args, client));
+            client.once(event.name, (...args) => event.execute(client, ...args));
         } else {
-            client.on(event.name, (...args) => event.execute(...args, client));
+            client.on(event.name, (...args) => event.execute(client, ...args));
         }
 
         console.log(`✔ Loaded Event: ${event.name}`);
     }
+
 };
