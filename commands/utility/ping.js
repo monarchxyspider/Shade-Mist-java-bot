f79b40b0054510b62a6ddc1b207daf0555fd69b8
monarchxyspider <raffaysweet@gmail.com>
2026-07-26 17:05:18 +0500

    Update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -3,22 +3,31 @@ const { EmbedBuilder } = require("discord.js");
 module.exports = {
     name: "ping",
     aliases: ["p"],
-    description: "Shows the bot latency.",
+    description: "Shows bot latency.",
 
     async execute(client, message) {
 
         const embed = new EmbedBuilder()
             .setColor(client.config.embedColor)
-            .setTitle("🏓 Pong!")
-            .setDescription(
-`**WebSocket Ping**
+            .setAuthor({
+                name: `${client.config.botName} • Bot Statistics`,
+                iconURL: client.user.displayAvatarURL()
+            })
+            .setThumbnail(client.user.displayAvatarURL())
+            .setDescription(`
+${client.config.emojis.stats} **Bot Statistics**
+
+${client.config.emojis.time} **WebSocket Ping**
 > \`${client.ws.ping}ms\`
 
-**Status**
-> Online`
-            )
+${client.config.emojis.gear} **Bot Status**
+> Online
+
+${client.config.emojis.success} **Successfully Responded**
+`)
             .setFooter({
-                text: client.config.botName
+                text: client.config.botName,
+                iconURL: client.user.displayAvatarURL()
             })
             .setTimestamp();
 
