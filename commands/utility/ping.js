3130e37952767aa63b0ee781a09603d38708028c
monarchxyspider <raffaysweet@gmail.com>
2026-07-26 15:41:33 +0500

    Update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,1 +1,34 @@
-// Ping Command
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+
+    name: "ping",
+    aliases: ["p"],
+    description: "Shows bot latency.",
+
+    async execute(client, message) {
+
+        const embed = new EmbedBuilder()
+            .setColor(client.config.embedColor)
+            .setAuthor({
+                name: `${client.config.botName} • Utility`
+            })
+            .setDescription(
+`${client.config.emojis.stats} **Bot Statistics**
+
+${client.config.emojis.time} **Latency**
+> \`${client.ws.ping}ms\`
+
+${client.config.emojis.gear} **Status**
+> Online
+
+✔️ **Request Completed Successfully**`
+            );
+
+        message.reply({
+            embeds: [embed]
+        });
+
+    }
+
+};
