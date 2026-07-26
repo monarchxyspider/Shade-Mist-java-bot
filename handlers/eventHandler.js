9790650b4a7a6d3b44240d9659294ad0c168edef
monarchxyspider <raffaysweet@gmail.com>
2026-07-26 13:29:52 +0500

    Update eventHandler.js

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -1,1 +1,22 @@
-1
+const fs = require("fs");
+const path = require("path");
+
+module.exports = (client) => {
+    const eventsPath = path.join(__dirname, "..", "events");
+
+    const files = fs
+        .readdirSync(eventsPath)
+        .filter(file => file.endsWith(".js"));
+
+    for (const file of files) {
+        const event = require(path.join(eventsPath, file));
+
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(...args, client));
+        } else {
+            client.on(event.name, (...args) => event.execute(...args, client));
+        }
+
+        console.log(`✔ Loaded Event: ${event.name}`);
+    }
+};
