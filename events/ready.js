047c66481de037da4ede334db827103d17da83d5
monarchxyspider <raffaysweet@gmail.com>
2026-07-26 16:00:10 +0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,10 +6,11 @@ module.exports = {
 
         console.clear();
 
-        console.log("====================================");
-        console.log(`🤖 Logged in as ${client.user.tag}`);
-        console.log(` ${client.config.botName} is Online!`);
-        console.log("====================================");
+        console.log("======================================");
+        console.log(`Logged in as : ${client.user.tag}`);
+        console.log(`Bot Name     : ${client.config.botName}`);
+        console.log("Status       : Online");
+        console.log("======================================");
 
         client.user.setPresence({
             status: "online",
