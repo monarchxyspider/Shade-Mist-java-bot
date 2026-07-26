11ff6290fcf909b3bf19ef13cde0c3f78764e558
monarchxyspider <raffaysweet@gmail.com>
2026-07-26 15:52:39 +0500

    Update commandHandler.js

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -2,27 +2,29 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = (client) => {
-    const commandsPath = path.join(__dirname, "..", "commands");
 
-    const folders = fs.readdirSync(commandsPath);
+    const folders = fs.readdirSync("./commands");
 
     for (const folder of folders) {
+
         const files = fs
-            .readdirSync(path.join(commandsPath, folder))
+            .readdirSync(path.join("./commands", folder))
             .filter(file => file.endsWith(".js"));
 
         for (const file of files) {
-            const command = require(path.join(commandsPath, folder, file));
+
+            const command = require(`../commands/${folder}/${file}`);
 
             client.commands.set(command.name, command);
 
             if (command.aliases) {
-                for (const alias of command.aliases) {
+                command.aliases.forEach(alias => {
                     client.aliases.set(alias, command.name);
-                }
+                });
             }
 
-            console.log(`✔ Loaded Command: ${command.name}`);
+            console.log(`✔ Loaded ${command.name}`);
         }
     }
+
 };
