40dcc3184226c791ec2e53ba802e0b2360ccf466
monarchxyspider <raffaysweet@gmail.com>
2026-07-27 11:36:00 +0500

    Update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -18,16 +18,16 @@ module.exports = {
 ${client.config.emojis.stats} __**Bot Statistics**__
 
 ${client.config.emojis.time} __**WebSocket**__
->>> \`${client.ws.ping}ms\`
+\`${client.ws.ping}ms\`
 
 ${client.config.emojis.gear} **Bot Status**
->>> <:upgrade:1494364520850784348> Online
+ <:upgrade:1494364520850784348> Online
 
 ${client.config.emojis.member} __**Guilds**__
->>> \`${client.guilds.cache.size}\`
+ \`${client.guilds.cache.size}\`
 
 ${client.config.emojis.user} __**Users**__
->>> \`${client.users.cache.size}\`
+ \`${client.users.cache.size}\`
 
 ${client.config.emojis.success} **Response**
 _** Successfully Responded**_
