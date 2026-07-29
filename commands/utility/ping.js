09fed75226f4fe1bb76dc4d31ac8ee80cd0f0b33
monarchxyspider <raffaysweet@gmail.com>
2026-07-29 14:39:19 +0500

    Update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -11,14 +11,6 @@ module.exports = {
 
     async execute(client, message) {
 
-        const row = new ActionRowBuilder().addComponents(
-            new ButtonBuilder()
-                .setLabel("Support Server")
-                .setEmoji("<:list:1494365866916053022>")
-                .setStyle(ButtonStyle.Link)
-                .setURL("https://discord.gg/54vJcse3")
-        );
-
         return message.reply({
             flags: 1 << 15,
             components: [
@@ -28,34 +20,50 @@ module.exports = {
                     components: [
                         {
                             type: 10,
-                            content: `## ${client.config.emojis.stats}  **${client.config.botName}**`
+                            content: `## ${client.config.emojis.stats} **${client.config.botName}**`
                         },
                         {
                             type: 10,
                             content: `Your all-in-one server Domain.`
                         },
-                        { type: 14 },
+                        {
+                            type: 14
+                        },
                         {
                             type: 10,
                             content: [
                                 `**Prefix:** \`s!\``,
                                 `**Help:** \`s!help\``,
                                 `**Server:** \`${message.guild.name}\``,
-                                `**Status:** <:upgrade:1494364520850784348> `,
-                                `$<:ping:1494366308601561310> **Ping:** \`${client.ws.ping}ms\``,
+                                `**Status:** <:upgrade:1494364520850784348>`,
+                                `<:ping:1494366308601561310> **Ping:** \`${client.ws.ping}ms\``,
                                 `${client.config.emojis.member} **Users:** \`${client.users.cache.size}\``
                             ].join("\n")
                         },
-                        { type: 14 },
+                        {
+                            type: 14
+                        },
                         {
                             type: 1,
                             components: [
                                 {
                                     type: 2,
-                                    label: "Support Server",
-                                    emoji: { id: "1494365866916053022", name: "list" },
                                     style: 5,
+                                    label: "Support Server",
+                                    emoji: {
+                                        id: "1494365866916053022",
+                                        name: "list"
+                                    },
                                     url: "https://discord.gg/uW34DMUu8"
+                                },
+                                {
+                                    type: 2,
+                                    style: 5,
+                                    label: "Add Bot",
+                                    emoji: {
+                                        name: "➕"
+                                    },
+                                    url: "https://discord.com/oauth2/authorize?client_id=1481550443149529270&permissions=8&integration_type=0&scope=bot+applications.commands"
                                 }
                             ]
                         }
