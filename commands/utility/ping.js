75537d3366f4c44d73a6bd5b6b683774d42387f6
monarchxyspider <raffaysweet@gmail.com>
2026-07-30 09:50:49 +0500

    Update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -38,7 +38,7 @@ module.exports = {
                                 `**Status:** <:upgrade:1494364520850784348>`,
                                 `<:ping:1494366308601561310> **Ping:** \`${client.ws.ping}ms\``,
                                 `${client.config.emojis.member} **Users:** \`${client.users.cache.size}\``,
-                                ``,         `**[Privacy](https://monarchxyspider.github.io/Shademist-docs/privacy.html)**`, `**•[Terms](https://monarchxyspider.github.io/Shademist-docs/terms.html)**`
+                                    `**[Privacy](https://monarchxyspider.github.io/Shademist-docs/privacy.html)**``**•[Terms](https://monarchxyspider.github.io/Shademist-docs/terms.html)**`
                             ].join("\n")
                         },
                         {
