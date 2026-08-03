78822b9fdfe7c9856bb655e7b57c32d7027ff77f
monarchxyspider <raffaysweet@gmail.com>
2026-08-03 09:40:52 +0500

    Update  welcome.js

--- Shinzo Den / welcome.js
+++ Shinzo Den / welcome.js
@@ -13,24 +13,20 @@ module.exports = {
     async execute(member) {
 
         const channel = member.guild.channels.cache.get(WELCOME_CHANNEL);
-
         if (!channel) return;
 
         const buttons = new ActionRowBuilder()
             .addComponents(
-
                 new ButtonBuilder()
                     .setStyle(ButtonStyle.Link)
                     .setLabel("Rules")
                     .setEmoji({ id: "1472901973400359007", animated: true })
                     .setURL("https://discord.com/channels/1470960049265574032/1470960050339184804"),
-
                 new ButtonBuilder()
                     .setStyle(ButtonStyle.Link)
                     .setLabel("Self Roles")
                     .setEmoji({ id: "1472901369060135033" })
                     .setURL("https://discord.com/channels/1470960049265574032/1526854844088975380"),
-
                 new ButtonBuilder()
                     .setStyle(ButtonStyle.Link)
                     .setLabel("Announcements")
@@ -46,16 +42,11 @@ module.exports = {
                     type: 17,
                     accent_color: 0xFF0000,
                     components: [
-
                         {
                             type: 10,
                             content: `# <a:w2:1482642554989318194> Welcome To The SHINZO_PLAYZ <a:RedCrown:1482388213300592662>`
                         },
-
-                        {
-                            type: 14
-                        },
-
+                        { type: 14 },
                         {
                             type: 10,
                             content:
@@ -70,11 +61,7 @@ module.exports = {
 > <a:ww:1484430425370656809> **Self Roles <:monarchs_list:1494580552366428310>**
 > __**☃️ [ゝʀᴏʟᴇꜱ⌝](https://discord.com/channels/1479436662248312915/1479436664911827027)**__`
                         },
-
-                        {
-                            type: 14
-                        },
-
+                        { type: 14 },
                         {
                             type: 10,
                             content:
@@ -82,7 +69,6 @@ module.exports = {
 <a:guitar:1486017856553357383> ***Thanks for joining our community!***
 <a:guitar:1486017856553357383> **Welcome to the Shinzo Army!** ${member}`
                         },
-
                         {
                             type: 12,
                             items: [
@@ -93,21 +79,17 @@ module.exports = {
                                 }
                             ]
                         },
-
                         {
                             type: 10,
                             content: `-# You are the **${member.guild.memberCount}** member of **Shinzo Den**.`
                         },
-
                         {
                             type: 1,
                             components: buttons.components
                         }
-
                     ]
                 }
             ]
         });
-
     }
 };
