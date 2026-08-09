94424d24e141a21f66dbed8ad77a0b5c58290f2d
monarchxyspider <raffaysweet@gmail.com>
2026-08-09 16:07:42 +0500

    Update welcomeButtons.js

--- commands/utility/welcomeutils/welcomebutton.js
+++ commands/utility/welcomeutils/welcomebutton.js
@@ -344,7 +344,7 @@ module.exports = async (client, interaction) => {
 
 
     // ==================================================
-    // EDIT THUMBNAIL
+    // EDIT THUMBAIL
     // ==================================================
 
     if (id === "welcome_edit_thumbnail") {
