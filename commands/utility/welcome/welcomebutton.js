d1110a8f9db723d1b975dad59914af5b9c5f7fda
monarchxyspider <raffaysweet@gmail.com>
2026-08-10 09:34:10 +0500

    Update welcomeButton.js

--- commands/utility/welcome/welcomebutton.js
+++ commands/utility/welcome/welcomebutton.js
@@ -19,7 +19,7 @@ module.exports = async function (client, interaction) {
     }
 
     // ==================================================
-    // CONFIG
+    // CONffIG
     // ==================================================
 
     if (!client.welcomeConfigs) {
